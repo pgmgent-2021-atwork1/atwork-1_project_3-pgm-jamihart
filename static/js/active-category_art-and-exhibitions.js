a78a136ab1ebf4f693c9ce589762b94b538dbc95
«pgm-jamihart» <jamihart@student.arteveldehs.be>
2021-01-15 01:48:56 +0100

    update

--- static/js/active-category_art-and-exhibitions.js
+++
@@ -1,8 +0,0 @@
-// let categoriesListLink = document.getElementsByClassName('categories__list-link');
-// let path = document.URL;
-
-// for (let i = 0; i < categoriesListLink.length; i++) {
-//   if (categoriesListLink[i].href === path) {
-//     categoriesListLink[i].className = 'category__list-link--active';
-//   }
-// }
